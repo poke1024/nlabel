359972d2f71ba7ed2ee097fffcdc750e225d3784
Bernhard Liebl <poke1024@gmx.de>
2022-02-16 12:48:57 +0100

    add external_key property

--- nlabel/__init__.py
+++ nlabel/__init__.py
@@ -9,6 +9,6 @@ import nlabel.embeddings
 from nlabel.nlp import NLP, Text
 from nlabel.io.common import to_path, open_archive as open, RemoteArchive
 from nlabel.io.slice import Slice
-from nlabel.io.json.property import tags, meta
+from nlabel.io.json.property import tags, meta, external_key
 from nlabel.io.json.loader import Document
 from nlabel.io.json.group import Group
